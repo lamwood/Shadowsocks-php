fe9555a5ba6c09bf09a22ea4d6ef207c08f7dd38
wood <960352880@qq.com>
2018-11-09 14:25:17 +0800

    添加支持IPV6

--- App/server.php
+++ App/server.php
@@ -151,23 +151,41 @@ $Worker->onMessage = function($connection, $buffer){
     +--------------+---------------------+------------------+----------+
  */
 function parse_socket5_header($buffer){
+    if(strlen($buffer) < 1){
+        return false;
+    }
     $addr_type = ord($buffer[0]);
     switch($addr_type){
         case ADDRTYPE_IPV4:
+            $header_length = 7;
+            if(strlen($buffer) < $header_length){
+                return false;
+            }
             $dest_addr = ord($buffer[1]).'.'.ord($buffer[2]).'.'.ord($buffer[3]).'.'.ord($buffer[4]);
             $port_data = unpack('n', substr($buffer, 5, 2));
             $dest_port = $port_data[1];
-            $header_length = 7;
             break;
         case ADDRTYPE_HOST:
+            if(strlen($buffer) < 2){
+                return false;
+            }
             $addrlen = ord($buffer[1]);
+            $header_length = $addrlen + 4;
+            if(strlen($buffer) < $header_length){
+                return false;
+            }
             $dest_addr = substr($buffer, 2, $addrlen);
             $port_data = unpack('n', substr($buffer, 2 + $addrlen, 2));
             $dest_port = $port_data[1];
-            $header_length = $addrlen + 4;
             break;
        case ADDRTYPE_IPV6:
-            echo 'todo ipv6 not support yet'."\n";
+            $header_length = 19;
+           if(strlen($buffer) < $header_length){
+               return false;
+           }
+           $dest_addr = inet_ntop(substr($buffer, 1, 16));
+           $port_data = unpack('n', substr($buffer, 17, 2));
+           $dest_port = $port_data[1];
             return false;
        default:
             echo 'unsupported addrtype '.$addr_type."\n";
